d2aac2a80b36b79d31c1a55f46c44ada1defd481
guojiao1993 <wang2012jida@163.com>
2018-07-15 14:40:03 +0800

    添加题意注释

--- src/J05_ReplaceSpaces/ReplaceSpaces.java
+++ src/J05_ReplaceSpaces/ReplaceSpaces.java
@@ -1,5 +1,10 @@
 package J05_ReplaceSpaces;
 
+/**
+ * 面试题5：替换空格
+ * <p>题目：请实现一个函数，把字符串中的每个空格替换成"%20"。例如输入“We are happy.”，
+ * 则输出“We%20are%20happy.”。
+ */
 public class ReplaceSpaces {
 
     public static String replaceSpace(StringBuffer str) {
