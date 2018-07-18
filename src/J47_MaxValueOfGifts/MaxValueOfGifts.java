e040d4edc27cc357924174f6b3340ce448efb45e
guojiao1993 <wang2012jida@163.com>
2018-07-18 21:03:46 +0800

    添加注释

--- src/J47_MaxValueOfGifts/MaxValueOfGifts.java
+++ src/J47_MaxValueOfGifts/MaxValueOfGifts.java
@@ -15,16 +15,21 @@ public class MaxValueOfGifts {
         }
         int length = board[0].length;
         int[] dp = new int[length];
+        // 先算行，再算列，比如算到第3行时，第1行存的值已经用不到了，所以只需一维数组，用来存上一行的记录
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 int left = 0;
+                // 当列数大于0时，才需要比较左边的记录[j-1]
                 if(j > 0) {
                     left = dp[j - 1];
                 }
                 int up = 0;
+                // 当行数大于0时，才需要比较上边的记录[j]
                 if(i > 0) {
                     up = dp[j];
                 }
+                // 左边的记录是当前行遍历时算出来的，上边的记录是存储值，当列数增加，则上边的记录j作废，
+                // 为重复利用，所以填充为当前行中，列数增一(j+1)后减一的列(j+1)-1=j的记录
                 dp[j] = Math.max(left, up) + board[i][j];
             }
         }
