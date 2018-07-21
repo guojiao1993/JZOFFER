5f5677f69dfb4e7140104e76e0c240235bf0d2a4
guojiao1993 <wang2012jida@163.com>
2018-07-21 20:31:18 +0800

    修正错误

--- src/J18_DeleteNodeInList/DeleteNodeInList.java
+++ src/J18_DeleteNodeInList/DeleteNodeInList.java
@@ -8,7 +8,7 @@ package J18_DeleteNodeInList;
 public class DeleteNodeInList {
     public ListNode deleteNode(ListNode head, ListNode tobeDelete) {
         // 如果只有表头节点，则删除表头节点
-        // 如果要删除的节点不是尾节点，则将后一节点覆盖要删除的节点，再删除该节点
+        // 如果要删除的节点不是尾节点，则将后一节点覆盖要删除的节点，再删除后一节点
         // 如果要删除的节点是尾节点，则从头结点遍历到尾节点的前一个节点，再删除尾节点
         return null;
     }
