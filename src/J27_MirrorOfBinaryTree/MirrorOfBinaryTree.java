4e3d7995b3e5bfe4362ac8d1df32bd723233df7d
guojiao1993 <wang2012jida@163.com>
2018-08-02 20:58:36 +0800

    增加第27题：二叉树的镜像

--- src/J27_MirrorOfBinaryTree/MirrorOfBinaryTree.java
+++ src/J27_MirrorOfBinaryTree/MirrorOfBinaryTree.java
@@ -1,4 +1,14 @@
 package J27_MirrorOfBinaryTree;
 
 public class MirrorOfBinaryTree {
+    public void Mirror(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
 }
---
+++ src/J27_MirrorOfBinaryTree/TreeNode.java
@@ -0,0 +1,11 @@
+package J27_MirrorOfBinaryTree;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
